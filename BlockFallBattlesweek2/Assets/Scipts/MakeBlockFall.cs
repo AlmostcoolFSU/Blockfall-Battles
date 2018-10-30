52fa55e2fd373bc16db7f501a0aeb7260e80433a
AlmostcoolFSU <44479998+AlmostcoolFSU@users.noreply.github.com>
2018-10-30 11:32:55 -0400

    Inital upload in week 2 class (After fixes)
    Inital upload in week 2 class (After fixes)

---
+++ BlockFallBattlesweek2/Assets/Scipts/MakeBlockFall.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBlockFall : MonoBehaviour
+{
+    private int speed = 2;
+    public bool Chosen = false, placed = false;
+    private string inputButton;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        if(transform.position.x < 0)
+        {
+            inputButton = "P1Block";
+        }
+       else if (transform.position.x > 0)
+        {
+            inputButton = "P2Block";
+        }
+
+
+        // mControler.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotL = new Vector3(0, 5, 0);
+        Vector3 rotR = new Vector3(0, -5, 0);
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(this.gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject child = this.transform.GetChild(0).gameObject;
+            child.GetComponent<Collider>().isTrigger = false;
+            child.GetComponent<Collider>().isTrigger = false;
+            child.GetComponent<Collider>().isTrigger = false;
+
+
+            //this.rb.isKinematic = true;
+        }
+        if (Chosen == true && placed == false && Input.GetKeyDown(KeyCode.Q))
+        {
+            //Use 90 degree turns get chuld 
+           // GameObject child = this.transform.GetChild(0).gameObject;
+           // child.transform.
+            Quaternion temp = transform.rotation;
+                temp.z += 0.5F; 
+                transform.rotation = temp;
+            Debug.Log("Rorate");
+        }
+        if (Chosen == true && placed == false && Input.GetKeyDown(KeyCode.E))
+        {
+            Quaternion temp = transform.rotation;
+            temp.z += 0.5F;
+            transform.rotation = temp;
+            Debug.Log("Rorate");
+        }
+        if (Input.GetButtonDown(inputButton) && !placed){
+ 
+     
+                this.Chosen = true;
+           
+        }
+
+    }
+    void OnMouseDown()
+    {
+        this.Chosen = true;
+    }
+    void FixedUpdate()
+    {
+       
+        if (Chosen == true && placed == false&& transform.position.x < 0)
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.GetComponent<Rigidbody>().isKinematic = false;
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3((moveHorizontal/4), 0.0f, 0.0f);
+            this.transform.position += movement * speed;
+            Debug.Log("use A,D");
+            if (placed == false)
+            {
+                transform.Translate(0, -(Time.deltaTime*speed), 0);
+            }
+        }
+        if (Chosen == true && placed == false && transform.position.x > 0)
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.GetComponent<Rigidbody>().isKinematic = false;
+            float moveHorizontal = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3((moveHorizontal / 4), 0.0f, 0.0f);
+            this.transform.position += movement * speed;
+            Debug.Log("use left,right");
+            if (placed == false)
+            {
+                transform.Translate(0, -(Time.deltaTime * 6), 0);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        MissionControl mControler = GameObject.Find("GameController").GetComponent<MissionControl>();
+        if (other.tag == "Ground" && placed != true || other.tag == "Block"&& placed != true)
+        {
+            placed = true;
+            Chosen = false;
+            if (mControler.turn == 1)
+            {
+                mControler.turn = 2;
+                Debug.Log("turn2 now");
+            }
+            else if (mControler.turn == 2)
+            {
+                mControler.turn = 1;
+                Debug.Log("turn1 now");
+            }
+            mControler.SpawnBlocks();
+            return;
+        }
+        
+    }
+}
---
+++ BlockFallBattlesweek2/Assets/Scipts/MissionControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionControl : MonoBehaviour {
+    public GameObject[] blocks,playingBlocks;
+    public GameObject Chosen;
+    public int rand1, rand2, rand3, turn=1;
+    public Transform pos1, pos2, pos3, pos4, pos5, pos6;
+    public bool start=false;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        // SpawnBlocks();
+       // Debug.Log("Something works");
+        SpawnBlocks();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("did it work?1");
+            playingBlocks = GameObject.FindGameObjectsWithTag("BlockHolder");
+            for (int i = 0; i< playingBlocks.Length; i++)
+            {
+                Debug.Log("Itteration");
+                Debug.Log(i);
+                rb =playingBlocks[i].GetComponent<Rigidbody>();
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                Debug.Log("did it work?2");
+               //GameObject.FindGameObjectsWithTag("BlockHolder").
+            }
+        }
+           
+    }
+    public void SpawnBlocks()
+    {
+        rand1 = Random.Range(0, blocks.Length);
+        rand2 = Random.Range(0, blocks.Length);
+        if (rand2 == rand1)
+        {
+            rand2 = Random.Range(0, blocks.Length);
+        }
+        rand3 = Random.Range(0, blocks.Length);
+        if (rand3 == rand2 || rand3 == rand1)
+        {
+            rand3 = Random.Range(0, blocks.Length);
+        }
+        if (turn == 1)
+        { 
+        Instantiate(blocks[rand1], pos1);
+            // Instantiate(blocks[rand2], pos2);
+            // Instantiate(blocks[rand3], pos3);
+        }
+        if (turn == 2)
+        {
+
+       
+         Instantiate(blocks[rand1], pos6);
+            // Instantiate(blocks[rand2], pos5);
+            // Instantiate(blocks[rand3], pos4);
+        }
+
+
+
+    }
+}
