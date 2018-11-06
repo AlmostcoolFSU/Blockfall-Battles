e03086c1a682492cb786bb890135cec80c057044
paintedeye <44612466+paintedeye@users.noreply.github.com>
2018-11-05 22:49:26 -0500

    prefab changes
    changed prefabs
    currently not working, not sure what I did wrong
    also added the material script which may or may not work

---
+++ BlockFallBattlesweek2/Assets/Scripts/GetMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMaterial : MonoBehaviour {
+
+    public Material[] randomMaterial;
+    //private Material mat;
+
+	void Start () {
+        //mat = GetComponent<Renderer>().material;
+        this.gameObject.GetComponent<Renderer>().material = randomMaterial[Random.Range(0, randomMaterial.Length)];
+    }
+}
